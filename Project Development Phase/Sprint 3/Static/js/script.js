8a6db76310af5086dc1130e3442b23003e7c5770
KarthigayanR <113588407+KarthigayanR@users.noreply.github.com>
2022-11-08 19:19:22 +0530

    Update script.js

--- Project Development Phase/Sprint 3/Static/js/script.js
+++ Project Development Phase/Sprint 3/Static/js/script.js
@@ -1,7 +1,6 @@
 const button = document.getElementById('button');
 const theForm = document.getElementById('theForm');
 const loading = document.getElementById('spinner');
-
 const disableButton = () => {
     console.log('Submitting form...');
     button.disabled = true;
@@ -9,7 +8,6 @@ const disableButton = () => {
     button.innerHTML = "Predicting..."
     loading.style.display = "block"
 };
-
 const enableButton = () => {
     console.log('Loading window...');
     button.disabled = false;
@@ -17,7 +15,5 @@ const enableButton = () => {
     button.innerHTML = "Predict"
     loading.style.display = "none"
 }
-
 theForm.onsubmit = disableButton;
-
 window.onload = enableButton;
